7171eb87615eb61098d11f8c068d21fa81c1b890
Bhaktesh604 <bhakteshgadhiya604@gmail.com>
2024-07-19 05:53:41 +0530

    add react-select

--- src/partials/dashboard/DashboardCard06.jsx
+++ src/partials/dashboard/DashboardCard06.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import DoughnutChart from '../../charts/DoughnutChart';
-
-// Import utilities
+import Select from 'react-select';
 import { tailwindConfig } from '../../utils/Utils';
 
-function DashboardCard06() {
+const statusOptions = [
+  { value: 'active', label: 'Active' },
+  { value: 'draft', label: 'Draft' }
+];
 
+const sizeOptions = [
+  { value: 'rings', label: 'Ring' },
+  { value: 'necklace', label: 'Necklace' },
+  { value: 'bracelet', label: 'Bracelet' },
+];
+
+const shapeOptions = [
+  { value: 'round', label: 'Round' },
+  { value: 'square', label: 'Square' },
+  { value: 'oval', label: 'Oval' },
+  { value: 'rectangle', label: 'Rectangle' },
+];
+
+
+function DashboardCard06() {
   const chartData = {
     labels: ['United States', 'Italy', 'Other'],
     datasets: [
       {
         label: 'Top Countries',
-        data: [
-          35, 30, 35,
-        ],
+        data: [35, 30, 35],
         backgroundColor: [
           tailwindConfig().theme.colors.indigo[500],
           tailwindConfig().theme.colors.blue[400],
@@ -31,172 +46,78 @@ function DashboardCard06() {
 
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
-      {/* <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
-        <h2 className="font-semibold text-slate-800 dark:text-slate-100">Top Countries</h2>
-      </header>  */}
       <div className="overflow-x-auto">
         <section className='listing px-6 py-6'>
           <div className="w-full">
-            <form className='w-full  '>
-
+            <form className='w-full'>
               <div className="row flex">
-                <label htmlFor="" className='p-0'>Status</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="Description" selected>Active</option>
-                  <option value="Media">Draft</option>
+                <div className='mb-3'>
+                  <label htmlFor="product-status" style={{ display: 'block', marginBottom: '10px' }}>Product Status</label>
+                  <Select
+                    id="product-status"
+                    name="status"
+                    options={statusOptions}
+                    className="basic-single-select"
+                    classNamePrefix="select"
+                  />
+                </div> 
+
+                <div className='mb-3'>
+                  <label htmlFor="shapes">Size</label>
+                  <Select
+                    id="shapes"
+                    defaultValue={[sizeOptions[2], sizeOptions[3]]}
+                    isMulti
+                    name="shapes"
+                    options={sizeOptions}
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                  />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor="shapes">Shape</label>
+                  <Select
+                    id="shapes"
+                    defaultValue={[shapeOptions[2], shapeOptions[3]]}
+                    isMulti
+                    name="shapes"
+                    options={shapeOptions}
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                  />
+                </div>
+
+                <label htmlFor="metal" className='p-0'>Metal</label>
+                <select id="metal" className="bg-gray-50 w-full border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option value="18KT" selected>18KT</option>
+                  <option value="14KT">14KT</option>
+                  <option value="10KT">10KT</option>
+                  <option value="925 Silver">925 Silver</option>
                 </select>
 
-                <label htmlFor="" className='p-0'>Ring Size</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="" selected>5</option>
-                  <option value="">6</option>
-                  <option value="">7</option>
-                  <option value="">8</option>
-                  <option value="">9</option>
-                  <option value="">10</option>
-                  <option value="">11</option>
-                  <option value="">12</option>
-                  <option value="">13</option>
-                  <option value="">14</option>
-                  <option value="">15</option>
-                  <option value="">16</option>
-                  <option value="">17</option>
-                  <option value="">18</option>
-                  <option value="">19</option>
-                  <option value="">20</option>
-                  <option value="">21</option>
-                  <option value="">22</option>
-                  <option value="">23</option>
-                  <option value="">24</option>
-                  <option value="">25</option>
+                <label htmlFor="weight" className='p-0'>Weight</label>
+                <select id="weight" className="bg-gray-50 w-full border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option value="Weight1" selected>Weight1</option>
+                  <option value="Weight2">Weight2</option>
                 </select>
 
-                <label htmlFor="" className='p-0'>Necklace Size</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value=" " selected>14 Inch</option>
-                  <option value=" ">16 Inch</option>
-                  <option value=" ">18 Inch</option>
-                  <option value=" ">20 Inch</option>
+                <label htmlFor="variant" className='p-0'>Variant</label>
+                <select id="variant" className="bg-gray-50 w-full border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option value="Variant1" selected>Variant1</option>
+                  <option value="Variant2">Variant2</option>
                 </select>
 
-                <label htmlFor="" className='p-0'>Bracelete Size</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value=" " selected>5 Inch</option>
-                  <option value=" ">6 Inch</option>
-                  <option value=" ">7 Inch</option>
-                  <option value=" ">8 Inch</option>
-                </select>
-
-
-                <label htmlFor="" className='p-0'>Primary Stone Shape</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="Description" selected>Round</option>
-                  <option value="Media">Square</option>
-                </select>
-
-
-                <label htmlFor="" className='p-0'>Metal</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="Description" selected>18KT</option>
-                  <option value="Media">14KT</option>
-                  <option value="Media">10KT</option>
-                  <option value="Media">925 Silver</option>
-                </select>
-                <label htmlFor="" className='p-0'>Weight</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="Description" selected>Active</option>
-                  <option value="Media">Draft</option>
-                </select>
-                <label htmlFor="" className='p-0'>Variant</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="Description" selected>Active</option>
-                  <option value="Media">Draft</option>
-                </select>
-
-                <label htmlFor="" className='p-0'>Collection</label>
-                <select id="countries" class="bg-gray-50 w-[100%] border mb-2  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option value="Description" selected>Active</option>
-                  <option value="Media">Draft</option>
-                </select>  
-              </div>
-              {/* <div className="row flex">
-                <select id="countries" class="bg-gray-50 w-[370px] border me-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>Collection</option>
-                  <option value="Description">Description</option>
-                  <option value="Media">Media</option>
-                  <option value="Collection">Collection</option>
-                  <option value="Pricing">Pricing</option>
-                  <option value="Stock">Stock</option>
-                  <option value="SKU">SKU</option>
-                  <option value="Weight">Weight</option>
-                  <option value="Variant">Variant</option>
-                </select>
-                <select id="countries" class="bg-gray-50 w-[370px] border me-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>Pricing</option>
-                  <option value="Description">Description</option>
-                  <option value="Media">Media</option>
-                  <option value="Collection">Collection</option>
-                  <option value="Pricing">Pricing</option>
-                  <option value="Stock">Stock</option>
-                  <option value="SKU">SKU</option>
-                  <option value="Weight">Weight</option>
-                  <option value="Variant">Variant</option>
+                <label htmlFor="collection" className='p-0'>Collection</label>
+                <select id="collection" className="bg-gray-50 w-full border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option value="Collection1" selected>Collection1</option>
+                  <option value="Collection2">Collection2</option>
                 </select>
               </div>
-              <div className="row flex">
-                <select id="countries" class="bg-gray-50 w-[370px] border me-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>Stock</option>
-                  <option value="Description">Description</option>
-                  <option value="Media">Media</option>
-                  <option value="Collection">Collection</option>
-                  <option value="Pricing">Pricing</option>
-                  <option value="Stock">Stock</option>
-                  <option value="SKU">SKU</option>
-                  <option value="Weight">Weight</option>
-                  <option value="Variant">Variant</option>
-                </select>
-                <select id="countries" class="bg-gray-50 w-[370px] border me-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>SKU</option>
-                  <option value="Description">Description</option>
-                  <option value="Media">Media</option>
-                  <option value="Collection">Collection</option>
-                  <option value="Pricing">Pricing</option>
-                  <option value="Stock">Stock</option>
-                  <option value="SKU">SKU</option>
-                  <option value="Weight">Weight</option>
-                  <option value="Variant">Variant</option>
-                </select>
-              </div>
-              <div className="row flex">
-                <select id="countries" class="bg-gray-50 w-[370px] border me-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>Weight</option>
-                  <option value="Description">Description</option>
-                  <option value="Media">Media</option>
-                  <option value="Collection">Collection</option>
-                  <option value="Pricing">Pricing</option>
-                  <option value="Stock">Stock</option>
-                  <option value="SKU">SKU</option>
-                  <option value="Weight">Weight</option>
-                  <option value="Variant">Variant</option>
-                </select>
-                <select id="countries" class="bg-gray-50 w-[370px] border me-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mt-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                  <option selected>Variant</option>
-                  <option value="Description">Description</option>
-                  <option value="Media">Media</option>
-                  <option value="Collection">Collection</option>
-                  <option value="Pricing">Pricing</option>
-                  <option value="Stock">Stock</option>
-                  <option value="SKU">SKU</option>
-                  <option value="Weight">Weight</option>
-                  <option value="Variant">Variant</option>
-                </select>
-              </div> */}
             </form>
           </div>
         </section>
       </div>
     </div>
-
   );
 }
 
